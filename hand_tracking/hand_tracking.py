0a1da1dadd44a8c3db9d574a7602b9bd159a9b19
Anil <anilmaity18@gmail.com>
2022-01-24 19:07:37 +0530

    D-H table

--- hand_tracking/hand_tracking.py
+++ hand_tracking/hand_tracking.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 import time
 import gc
 
+buttons = [False,False,False,False,False]
+
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
@@ -22,16 +24,43 @@ class handDetector():
                     self.mpDraw.draw_landmarks(image, hand, self.mpHands.HAND_CONNECTIONS)
         return image
     def findPosition(self, image, handNo=0, draw=True):
-        lmList = []
+        x = []
+        y = []
         h, w, c = image.shape
         if self.result.multi_hand_landmarks:
             myHand = self.result.multi_hand_landmarks[handNo]
             for id, land in enumerate(myHand.landmark):
                 cx, cy = int(land.x * w), int(land.y * h)
-                lmList.append([id, cx, cy])
+                x.append(cx)
+                y.append(cy)
+
                 if draw:
                     cv.circle(image, (cx,cy), 15, (0,0,255), cv.FILLED)
-        return lmList
+
+            self.button_condition(x,y)
+
+
+            print(buttons)
+
+        return x,y
+
+
+    def button_condition(self,x,y):
+        global buttons
+        l = [8,12,16,20]
+        l_c=[7,11,15,19]
+        d = []
+        d_c = []
+
+        for i in range(0,4):
+
+            d.append(abs(x[l[i]] - x[0]) + abs(y[l[i]] - y[0]))
+            d_c.append(abs(x[l_c[i]] - x[0]) + abs(y[l_c[i]] - y[0]))
+            # print(d_1,d_c_1)
+            if d[i] > d_c[i]:
+                buttons[i] = True
+            else:
+                buttons[i] = False
 
 
 def main():
