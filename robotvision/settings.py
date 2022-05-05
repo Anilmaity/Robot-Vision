884d6fe7c9f0951964fad462c3acccdc411068d0
Anil <anilmaity18@gmail.com>
2022-05-05 22:12:55 +0530

    bugs

--- robotvision/settings.py
+++ robotvision/settings.py
@@ -122,12 +122,9 @@ STATIC_DIRS = [os.path.join(BASE_DIR, 'static')],
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
