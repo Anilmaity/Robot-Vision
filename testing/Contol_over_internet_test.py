6cbc3191c0545ebe865074997b4274b2beaca0a8
anilmaity <anilmaity77@gmail.com>
2022-05-07 16:46:06 +0100

    tested_in_pi

--- testing/Contol_over_internet_test.py
+++ testing/Contol_over_internet_test.py
@@ -6,16 +6,14 @@ myobj = {'id_no': '00'}
 joints =[0,0,0,0,0,0]
 
 
-
-import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-stepen = 8
-stepper_sig_pin = [7]
-stepper_dir_pin = [1]
+stepen = 26
+stepper_sig_pin = [13,5,20]
+stepper_dir_pin = [19,6,16]
 GPIO.setup(stepen, GPIO.OUT)
 
 
@@ -25,31 +23,35 @@ GPIO.setup(stepen, GPIO.OUT)
 def step_setup():
     for pin in stepper_sig_pin:
         GPIO.setup(pin, GPIO.OUT)
+        print(pin)
     for pin in stepper_dir_pin:
         GPIO.setup(pin, GPIO.OUT)
-
+        print(pin)
     GPIO.output(stepen, GPIO.HIGH)
-    time.sleep(0.1)
+    time.sleep(2)
     GPIO.output(stepen, GPIO.LOW)
 
 
 def step_run(steps, stepper_no):
-    for step in  steps:
-        if step >= 0:
-            GPIO.output(stepper_dir_pin[stepper_no], GPIO.HIGH)
-            for i in range(0, 1):
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.LOW)
+    for i,stepper in  enumerate(stepper_no):
+        if steps[i] >= 0:
+            GPIO.output(stepper_dir_pin[stepper], GPIO.HIGH)
+            for i in range(0, steps[i]):
+                GPIO.output(stepper_sig_pin[stepper], GPIO.LOW)
                 time.sleep(0.001)
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.HIGH)
+                GPIO.output(stepper_sig_pin[stepper], GPIO.HIGH)
                 time.sleep(0.001)
+            print("complete_cw")
 
-        elif step < 0:
-            GPIO.output(stepper_dir_pin[stepper_no], GPIO.LOW)
-            for i in range(0, steps):
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.LOW)
+        elif steps[i] < 0:
+            GPIO.output(stepper_dir_pin[stepper], GPIO.LOW)
+            for i in range(0, abs(steps[i])):
+                GPIO.output(stepper_sig_pin[stepper], GPIO.LOW)
                 time.sleep(0.001)
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.HIGH)
+                GPIO.output(stepper_sig_pin[stepper], GPIO.HIGH)
                 time.sleep(0.001)
+            print("complete_ccw")
+
 
 
 step_setup()
@@ -58,11 +60,11 @@ while True:
     x = requests.post(url, data=myobj)
     data = x.json()
 
-    stepper_delta_angle = [joints[0] - data['stepper_1'] , joints[1] - data['stepper_2'], joints[2] - data['stepper_3']]
+    stepper_delta_angle = [joints[0] - (data['stepper_1']*26) , joints[1] - (data['stepper_2']*26), joints[2] - (data['stepper_3']*26)]
 
-    joints[0] = data['stepper_1']
-    joints[1] = data['stepper_2']
-    joints[2] = data['stepper_3']
+    joints[0] = data['stepper_1']*26
+    joints[1] = data['stepper_2']*26
+    joints[2] = data['stepper_3']*26
     joints[3] = data['servo_1']
     joints[4] = data['servo_2']
     joints[5] = data['servo_3']
--- testing/stepper_test.py
+++ testing/stepper_test.py
@@ -1,12 +1,13 @@
+
 import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-stepen = 8
-stepper_sig_pin = [7]
-stepper_dir_pin = [1]
+stepen = 26
+stepper_sig_pin = [13,5,20]
+stepper_dir_pin = [19,6,16]
 GPIO.setup(stepen, GPIO.OUT)
 
 
@@ -16,36 +17,48 @@ GPIO.setup(stepen, GPIO.OUT)
 def step_setup():
     for pin in stepper_sig_pin:
         GPIO.setup(pin, GPIO.OUT)
+        print(pin)
     for pin in stepper_dir_pin:
         GPIO.setup(pin, GPIO.OUT)
-
+        print(pin)
     GPIO.output(stepen, GPIO.HIGH)
-    time.sleep(0.1)
+    time.sleep(2)
     GPIO.output(stepen, GPIO.LOW)
 
 
 def step_run(steps, stepper_no):
-    for step in  steps:
-        if step >= 0:
-            GPIO.output(stepper_dir_pin[stepper_no], GPIO.HIGH)
-            for i in range(0, 1):
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.LOW)
-                time.sleep(0.001)
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.HIGH)
-                time.sleep(0.001)
+    for i,stepper in  enumerate(stepper_no):
+        print(stepper_dir_pin[stepper])
+        print(stepper_sig_pin[stepper])
 
-        elif step < 0:
-            GPIO.output(stepper_dir_pin[stepper_no], GPIO.LOW)
-            for i in range(0, steps):
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.LOW)
+        print(steps[i])
+        if steps[i] >= 0:
+            GPIO.output(stepper_dir_pin[stepper], GPIO.HIGH)
+            for i in range(0, steps[i]):
+                GPIO.output(stepper_sig_pin[stepper], GPIO.LOW)
                 time.sleep(0.001)
-                GPIO.output(stepper_sig_pin[stepper_no], GPIO.HIGH)
+                GPIO.output(stepper_sig_pin[stepper], GPIO.HIGH)
                 time.sleep(0.001)
+            print("complete_cw")
+
+        elif steps[i] < 0:
+            GPIO.output(stepper_dir_pin[stepper], GPIO.LOW)
+            for i in range(0, abs(steps[i])):
+                GPIO.output(stepper_sig_pin[stepper], GPIO.LOW)
+                time.sleep(0.001)
+                GPIO.output(stepper_sig_pin[stepper], GPIO.HIGH)
+                time.sleep(0.001)
+            print("complete_ccw")
+
 
 
 step_setup()
-while(True):
-    step_run(200, 0)
-    time.sleep(0.5)
-    step_run(200, 0)
-    time.sleep(0.5)
+
+while True:
+    
+
+    step_run([400], [2])
+    time.sleep(1)
+    step_run([-400], [2])
+
+
