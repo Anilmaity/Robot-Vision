11976fc744517e673f2a6d66e24ebdf7c7ba0adf
Anil <anilmaity18@gmail.com>
2022-05-09 14:19:48 +0530

    servo code added to test

--- testing/servo_control.py
+++ testing/servo_control.py
@@ -4,31 +4,39 @@ import time
 
 
 
-servoPIN = 17
+servopin = [1,7,8,25]# 1, 7, 8, 25
+servolist = []
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+for i, s_p in enumerate(servopin):
+  GPIO.setup(s_p, GPIO.OUT)
+  servolist.append(GPIO.PWM(s_p, 50)) # GPIO 17 for PWM with 50Hz
+  servolist[i].start(2.5) # Initialization
+
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+
+    for serv in servolist:
+
+      serv.ChangeDutyCycle(5)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(7.5)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(10)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(12.5)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(10)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(7.5)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(5)
+      time.sleep(0.5)
+      serv.ChangeDutyCycle(2.5)
+      time.sleep(0.5)
+
 except KeyboardInterrupt:
-  p.stop()
+  for serv in servolist:
+    serv.stop()
   GPIO.cleanup()
 
