db66790a9839c4851a711ca77f85de87f2574335
Anil <anilmaity18@gmail.com>
2022-05-25 20:11:39 +0530

    api fix

--- api/apis/get_angles.py
+++ api/apis/get_angles.py
@@ -9,14 +9,18 @@ from api.models import Robot
 from api.serializers import RobotSerializer
 
 class get_angles(APIView):
-    authentication_classes = [TokenAuthentication]
     permission_classes = [AllowAny]
 
     def post(self, request):
+        print(request.data)
         id = request.data['id_no']
+        print(id)
 
         robot = Robot.objects.get(id_no=id)
-
+        print(robot)
         ser = RobotSerializer(robot)
-        return JsonResponse(ser.data, safe=False)
+        print(ser.data)
+
+        return Response(ser.data, status=200)
+        #return JsonResponse(ser.data, safe=False)
 
