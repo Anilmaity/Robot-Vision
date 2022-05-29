98df89a39253362425485dacb25dae7f66c7d4bc
Anil <anilmaity18@gmail.com>
2022-05-29 17:57:14 +0530

    log update code added

--- api/apis/get_angles.py
+++ api/apis/get_angles.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import os
 url = 'https://robotvision.herokuapp.com/get_angles/'
 myobj = {'id_no': '00'}
 
@@ -7,7 +8,39 @@ x = requests.post(url, data = myobj)
 
 print(x.text)
 
-while(True):
-    x = requests.post(url, data=myobj)
-    print(x.text)
-    time.sleep(1)
+
+def write_to_file(file_name, data):
+    f = open(file_name, 'a')
+    now = time.strftime("%c")
+    f.write(str(now) + "  " + str(data) + "\n")
+    f.close()
+
+
+# create a function to check file exist or not
+def check_file_exist(file_name):
+    if os.path.isfile(file_name):
+        return True
+    else:
+        return False
+
+
+# create a function to create file
+def create_file(file_name, data):
+    if check_file_exist(file_name):
+        write_to_file(file_name, data)
+    else:
+        f = open(file_name, 'w')
+        f.close()
+        write_to_file(file_name, data)
+
+print(time.strftime("%d-%m-%Y"))
+try:
+    while (True):
+        x = requests.post(url, data=myobj)
+        print(x.text)
+        todays_date = time.strftime("%d-%m-%Y")
+        create_file('Logs\\'+str(todays_date), x.text)
+
+except KeyboardInterrupt:
+        print("\nExiting...")
+
--- hand_tracking/cv_video_input.py
+++ hand_tracking/cv_video_input.py
@@ -1,5 +1,5 @@
 import cv2
-
+import os
 url = "http://192.168.43.1:8080/video"
 cap = cv2.VideoCapture(url)
 
@@ -13,3 +13,20 @@ while True:
     if q == ord("q"):
         break
 cv2.destroyAllWindows()
+
+
+def create_log_file(file_name):
+    # check log file
+    if not os.path.exists(file_name):
+        # create log file
+
+        with open(file_name, "w") as f:
+            f.write("")
+    else:
+        log_file = open(file_name, "a")
+        log_file.write("\n")
+        log_file.close()
+
+
+    with open(file_name, "w") as f:
+        f.write("")
