c4220c32c04dc01434de497f6bc8a0949df6bc8a
Anil <anilmaity18@gmail.com>
2022-06-27 00:00:16 +0530

    update bug fix host changes

--- robotvision/settings.py
+++ robotvision/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#^)iyc@ssff(bbg05o4knn=da94q6ds6gq&819&i8sp4-fp%t(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
